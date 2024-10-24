8d414fba0834170947aa696e6bcef2c3eca945db
opdums <operez6921@gmail.com>
2024-10-24 08:57:41 -0500

    feature/ñogin

--- e2e/tour operacion/login.spec.ts
+++ e2e/tour operacion/login.spec.ts
@@ -9,8 +9,14 @@ test.describe('Como automatizador quiero crear casos de pruebas para el login', 
     test.beforeEach(async ({ page }) => {
         await page.goto(variables.urlBase)
     })
+ 
+    test.afterEach(async ({ page }) => {
+        await page.context().cookies(variables.urlBase);
+        await page.context().clearCookies()
+        await page.close()
+    })
     
-    test(' Inicio de Sesión exitoso', async ({ page }) => {
+    test('Inicio de Sesión exitoso', async ({ page }) => {
         await test.step('ingreso las credenciales emial y contraseña', async () => {
             await page.locator('#user').fill(variables.userName)
             await page.locator('#password').fill(variables.password)
@@ -24,23 +30,19 @@ test.describe('Como automatizador quiero crear casos de pruebas para el login', 
     })    
 
     test('Inicio de Sesión Incorrecto', async ({ page }) => {
-        test.info().annotations.push({
-            type: 'Bug',
-            description: 'Falta visualizar la alerta que le indica que la informacion no corresponde'
-        })
-
-        await test.step('ingreso las credenciales emial y contraseña incorrectos', async () => {
+        await test.step('ingreso las credenciales usuario y contraseña incorrectos', async () => {
             await page.locator('#user').fill(variables.userName)
-            await page.locator('#password').fill('123123222222')
+            await page.locator('#password').fill(variables.passwordError)
         })
 
-        await test.step('el sistema permita ser redirigido a la página principal', async () => {
+        await test.step('el sistema debe mostrar un mensaje de error indicando que el usuario ingreso mal la contraseña', async () => {
             await page.getByRole('button', { name: 'Entrar'}).click()
-    
+            let validacion = await page.locator('//html/body/div/div/div[2]/form/p').textContent();
+            expect(validacion).toEqual(validacion)
         })
     })
 
-    test('Intento de Inicio de Sesión con Campos Vacíos', async ({ page }) => {
+    test('Inicio de Sesión con Campos Vacíos', async ({ page }) => {
         await test.step('Dado que el usuario no ingresa las crdenciales', async () => {
             await page.locator('#user').fill(variables.userName)
             await page.locator('#user').clear()
@@ -55,19 +57,16 @@ test.describe('Como automatizador quiero crear casos de pruebas para el login', 
         })  
     })
 
-    test('Inicio de Sesión con Usuario No Existente', async ({ page }) => {
-        test.info().annotations.push({
-            type: 'Bug',
-            description: 'Falta visualizar la alerta que le usuairo no existe'
-        })
-        
+    test('Inicio de Sesión con Usuario No Existente', async ({ page }) => {        
         await test.step('ingreso las credenciales emial y contraseña incorrectos', async () => {
-            await page.locator('#user').fill('noexisto@gmail.com')
-            await page.locator('#password').fill('123123222222a')
+            await page.locator('#user').fill(variables.emailError)
+            await page.locator('#password').fill(variables.passwordError)
         })
         
-        await test.step('el sistema debe mostrar un mensaje de error indicando que el usuario no existe ', async () => {
+        await test.step('el sistema debe mostrar un mensaje de error indicando que el usuario no existe', async () => {
             await page.getByRole('button', { name: 'Entrar'}).click()
+            let validacion = await page.locator('//html/body/div/div/div[2]/form/p').textContent();
+            expect(validacion).toEqual(validacion)
         })
     })
 })
